12e3fd96ad1d7c14a525691501b5fa5db72360a8
Tanya627 <tanyaocampo96@yahoo.com>
2021-10-26 01:24:12 +0000

    - had issues with using live server in the dev contianer - copied and pasted the new code form offline soruce - worked on the abc buttons - created counters to keep track of user progress - making the buttons display the letters they represent. -css changes to the buttons -html id and class name changes and minor reorganization

--- src/index.js
+++ src/index.js
@@ -16,39 +16,77 @@ const secretWordArray = [
   "VENEZUELA",
 ];
 // set the computers pick to the secret word
-let secretWord =
-  secretWordArray[Math.floor(Math.random() * secretWordArray.length)];
-console.log(secretWord);
-//TODO: Change random word to lower case
-//TODO: this is the section that needs to display as dashes for the user
-document.querySelector(".theword").textContent = secretWord;
-//displays the count of the leters in the word
+let secretWord = "";
+let maxWrong = 7;
+let mistakes = 0;
+let guessed = [];
+let clickedLetter = null;
+//selecting random word
+const randomWord = () => {
+  secretWord =
+    secretWordArray[Math.floor(Math.random() * secretWordArray.length)];
+  console.log(secretWord);
+};
+randomWord();
+// generate the functions for the keyboard
+const generateButtons = () => {
+  let buttonsHtml = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    .split("")
+    .map(
+      (letter) =>
+        `
+      <button class="btn btn-lg btn-primary m-2"
+      id='` +
+        letter +
+        `'
+      onClick ="handleGuess('` +
+        letter +
+        `')">
+      ` +
+        letter +
+        `
+      </button>
+    `
+    )
+
+    .join("");
+  document.getElementById("keyboard").innerHTML = buttonsHtml;
+};
+generateButtons();
+
+// working with the max wrong
+document.getElementById("max-wrong").innerHTML = maxWrong;
+// working with word status
+const handleGuess = (chosenLetter) => {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute("disabled", true);
+  //alert(secretWord);
+  if (secretWord.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+  } else if (secretWord.indexOf(chosenLetter) === -1) {
+    mistakes++;
+    updateMistakes();
+  }
+};
+
+// TODO have clicked buttons darken when clicked
+//TODO make the reset button refresh the page
+//Checking if the guessed letter is in the secret word
+const guessedWord = () => {
+  let clickedLetter = secretWord
+    .split("")
+    .map((letter) => (guessed.indexOf(letter) >= 0 ? letter : " _ "))
+    .join("");
+  document.getElementById("the-words").innerHTML = clickedLetter;
+  console.log(clickedLetter);
+};
+guessedWord();
+// Updating the counter/ mistakes
+const updateMistakes = () => {
+  document.getElementById("mistakes").innerHTML = mistakes;
+};
+
+// TODO Display yes if letter is found display no if not
 document.querySelector(
   ".lettercount"
 ).textContent = `Your secret word has: ${secretWord.length} letters!`;
-
-
-
-const letterChecker = (selectedWord) => {
-  let moves = 0;
-  for (let i = 0; i < selectedWord.length; i++) {
-    let currentLetter = selectedWord[i];
-    if (currentLetter === loweruserInputLetter) {
-      console.log("YES!");
-      moves++;
-      moves <= 1
-        ? console.log(`You have ${moves} correct letter!`)
-        : console.log(`You have ${moves} correct letters!`);
-    }
-  }
-};
-console.log(letterChecker(secretWord));
-// for every time that the user clicks, we will run letterChecker until user is out of clicks/letter buttns. Using recursion ?
-
-// we then plan on using that vairiable to define the fowllowing:
-
-// the letter exist in the word
-
-// Letter does not exist in the word
-
-// the letter has been used
